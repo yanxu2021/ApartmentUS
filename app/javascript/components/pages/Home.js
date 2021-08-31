c580906b09359af08dacfacb8b2afc4f9e9b7f0f
yanxu2021 <yan.xu2021@outlook.com>
2021-08-31 15:27:45 -0700

    complete setup

---
+++ app/javascript/components/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react"
+class Home extends React.Component {
+  render() {
+    return(
+      <h3>This is the Home Page</h3>
+    )
+  }
+}
+export default Home
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
+  root 'home#index'
 end
